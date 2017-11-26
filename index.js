3a3a3be59f7f6e89f6c723c0102c5f4dea1c07fa
Deepak54321 <31242754+Deepak54321@users.noreply.github.com>
2017-11-26 08:12:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ restService.use(bodyParser.json());
 function mymethod(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     var demo="Demo New";
-    res.json({'speech': 'When',
+    var action=req.get("result").get("action");	
+	switch(action)
+	       {
+		       case "demo":
+			        res.json({'speech': 'When',
               'displayText': 'When',
               'messages': 
               [
@@ -27,20 +31,40 @@ function mymethod(req, res) {
                 'type': 2}
               ],
               'source': 'dimwei.com'});
-}
-function pprice(req,res)
-{
-	               console.log("%s","Product Price Called");
-		       var price="51000 Rs";
-                       return res.json({
-                       speech: price,
-                       displayText: price,
-                       source: 'webhook-echo-sample'
-                       });             
-}
+			       break;
+			     case "price"
+			       var request = require('request');
+            request({
+                url:'http://www.yamaha-motor-india.com/iym-web-api//51DCDFC2A2BC9/statewiseprice/getprice?product_profile_id=salutorxspcol&state_id=240'
+            },function (error,response,body) {
+                if (!error && response.statusCode == 200) {
+                    var result = JSON.parse(body);
+                    var responseCode=result.responseData;
+                    var productPrice=responseCode.product_price;
+                    var price=productPrice[0].price +'Rs';
+                   // var webhookReply = 'Hello ' + userName + '! Welcome from the webhook.'
+
+  // the most basic response
+                res.status(200).json({
+                source: 'webhook',
+                speech: price,
+                displayText: price
+           });
+                }
+                else {
+                    console(log.error());
+                }
+            });
+			       break;
+			        default:
+			       console.log("abc");
+	       }
+			       
+   }
+
 restService.post('/echo', mymethod);
 
-restService.post('/echo',pprice);
+//restService.post('/echo',pprice);
 
 restService.post('/slack-test', function(req, res) {
 
